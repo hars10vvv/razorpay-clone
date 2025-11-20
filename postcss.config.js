0212baadfe822cbbb9495233bbdc32052553c9c9
hars10vvv <hars10vvv@gmail.com>
2025-11-20 14:02:49 +0530

    more update

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,6 @@
 export default {
-  content: ["*"],
-  theme: {
-    extend: {
-      fontFamily: {
-        mullish: ["mullish", "sans-serif"],
-      },
-      colors: {
-        deepBlue: "#02042a",
-        lightBlue: "#2b84ea",
-        lightBlue300: "#4b94ed",
-        lightBlue500: "#0b72e7",
-        greenLight: "#61cea6",
-        grayText: "#818597",
-        lightGray: "#e2e2e2",
-        grayBlue: "#344abc",
-        deepBlueHead: "#162f56",
-        gray2: "#525a76",
-      },
-    },
-  },
-  plugins: {
-    "@tailwindcss/postcss": {},
+  plugins: { "@tailwindcss/postcss": {} },
+  extends: {
+    fontFamily: { mullish: ["mullish", "sans-serif"] },
   },
 };
