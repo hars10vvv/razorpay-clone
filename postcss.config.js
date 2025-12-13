5cec7dd3d6cc0dcc7485a2b46a38c1fbe77da5a5
hars10vvv <hars10vvv@gmail.com>
2025-12-13 15:51:55 +0530

    ean02

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,17 @@ export default {
   plugins: { "@tailwindcss/postcss": {} },
   extends: {
     fontFamily: { mullish: ["mullish", "sans-serif"] },
+    color: {
+      deepBlue: "#02042a",
+      lightBlue: "#2b84ea",
+      lightBlue300: "#4b94ed",
+      lightBlue500: "#0b72e7",
+      greenLight: "#61cea6",
+      grayText: "#818597",
+      lightGray: "#e2e2e2",
+      grayBlue: "#344a6c",
+      deepBlueHead: "#162f56",
+      gray2: "#525a76",
+    },
   },
 };
